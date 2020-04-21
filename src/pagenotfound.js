48c551e6a6b8f7a902ef32d4100d0b853b9dea7f
Kalaiarasan Pushpanathan <kalaiarasan0215@gmail.com>
2020-04-21 22:46:39 +0530

    Fixed 404 class issue

--- src/pagenotfound.js
+++ src/pagenotfound.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="error-template">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="error-template">
                         <h1>
                             Oops!</h1>
                         <h2>
                             404 Not Found</h2>
-                        <div class="error-actions">
-                            <Link to="/" class="btn btn-primary btn-lg">
-                                <span class="glyphicon glyphicon-home"></span>Take Me Home
+                        <div className="error-actions">
+                            <Link to="/" className="btn btn-primary btn-lg">
+                                <span className="glyphicon glyphicon-home"></span>Take Me Home
                             </Link>
                         </div>
                     </div>
